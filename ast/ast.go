d75557c9be22feb5a19c433ff0e28b0189eedf2e
cagri yildirim <cagri.yildirim.r@gmail.com>
2025-03-09 14:05:02 +0300

    🐵 Add basic AST types

---
+++ ast/ast.go
@@ -0,0 +1,56 @@
+package ast
+
+import "github.com/cagriyildirimr/ape/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode() // convenience method for go compiler to catch if type is statement
+}
+
+type Expression interface {
+	Node
+	expressionNode() // convenience method
+}
+
+// Program is a list of Statement
+// Program is the root Node of our language
+// therefore it needs to implement Node
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (l *LetStatement) statementNode() {}
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
+}
+
+// Identifier is an expression not a statement
+// even though it doesn't produce a value
+// because later we will use some identifier
+// in right hand side of the let statement
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
