b81107eb91dfe09831836bd744103f0e4207e289
cagri yildirim <cagri.yildirim.r@gmail.com>
2025-03-09 16:08:22 +0300

    🐵 Refactor parser to align with book

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/cagriyildirimr/ape/ast"
 	"github.com/cagriyildirimr/ape/lexer"
 	"github.com/cagriyildirimr/ape/token"
@@ -12,10 +13,11 @@ type Parser struct {
 	l            *lexer.Lexer
 	currentToken token.Token
 	peekToken    token.Token
+	errors       []string
 }
 
 func New(lexer *lexer.Lexer) *Parser {
-	p := &Parser{l: lexer}
+	p := &Parser{l: lexer, errors: make([]string, 0)}
 
 	p.NextToken()
 	p.NextToken()
@@ -31,28 +33,76 @@ func (p *Parser) NextToken() {
 func (p *Parser) ParseProgram() *ast.Program {
 	program := ast.NewProgram()
 
-	for !(p.currentToken.Type == token.SEMICOLON && p.peekToken.Type == token.EOF) {
-		if p.currentToken.Type == token.LET {
-			program.AddStatement(p.parseLet())
-		} else {
-			p.NextToken()
+	for p.currentToken.Type != token.EOF {
+		stmt := p.ParseStatement()
+		if stmt != nil {
+			program.AddStatement(stmt)
 		}
+		p.NextToken()
 	}
 	return program
 }
 
+func (p *Parser) ParseStatement() ast.Statement {
+	switch p.currentToken.Type {
+	case token.LET:
+		return p.parseLet()
+	default:
+		return nil
+	}
+}
+
 func (p *Parser) parseLet() ast.Statement {
 	letToken := p.currentToken
-	if p.peekToken.Type != token.IDENT {
+
+	if !p.expectPeek(token.IDENT) {
 		return nil
 	}
-	ident := ast.NewIdentifier(p.peekToken)
-	p.NextToken() // current token is ident next token is equal
-	if p.peekToken.Type != token.ASSIGN {
+
+	ident := ast.NewIdentifier(p.currentToken)
+
+	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
-	p.NextToken() // expr
-	intExp := ast.NewIntExpression(p.peekToken)
+
+	for !p.expectPeek(token.INT) { // later add more complex expressions
+		return nil
+	}
+	intExp := ast.NewIntExpression(p.currentToken)
 	letStatement := ast.NewLetStatement(letToken, ident, intExp)
+
+	if !p.expectPeek(token.SEMICOLON) {
+		return nil
+	}
 	return letStatement
+}
+
+func (p *Parser) expectPeek(t token.Type) bool {
+	if p.peekTokenIs(t) {
+		p.NextToken()
+		return true
+	}
+	p.peekError(t)
+	return false
+}
+
+func (p *Parser) currentTokenIs(t token.Type) bool {
+	return p.currentToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.Type) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.Type) {
+	p.errors = append(p.errors, p.createErrorMessage(t, p.peekToken.Type))
+}
+
+func (p *Parser) createErrorMessage(got, want token.Type) string {
+	return fmt.Sprintf("expected next token to be %s, got %s instead",
+		got, want)
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,8 +1,11 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/cagriyildirimr/ape/ast"
 	"github.com/cagriyildirimr/ape/lexer"
+	"github.com/cagriyildirimr/ape/token"
+	"strings"
 	"testing"
 )
 
@@ -16,6 +19,7 @@ let foobar = 42;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -74,4 +78,55 @@ func testLetStatement(t *testing.T, s ast.Statement, name string, expression str
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
+
+func TestLetStatementsErrors(t *testing.T) {
+	input := `
+let x 5;
+let  = 10;
+let  42;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	tests := []struct {
+		want token.Type
+		got  token.Type
+	}{
+		{token.ASSIGN, token.INT},
+		{token.IDENT, token.ASSIGN},
+		{token.IDENT, token.INT},
+	}
+	errors := p.Errors()
+	if len(errors) != len(tests) {
+		t.Fatalf("expected %d errors, got %d", len(tests), len(errors))
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("Error %d", i), func(t *testing.T) {
+			actual := errors[i]
+			if !strings.EqualFold(actual, p.createErrorMessage(test.want, test.got)) {
+				t.Errorf("error")
+			}
+		})
+	}
 }
